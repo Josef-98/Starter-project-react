b8e89f84a7ce4c78b392aa9dc076ef9076b8dd27
unknown <youssef.chamekh98@gmail.com>
2021-03-25 10:30:48 +0100

    clone amina repo

---
+++ src/router/router.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import DashboardPage from "../pages/dashboard-page ";
+import Error404Page from "../pages/Error 404 page";
+import { AuthProvider } from "../shared/auth/Auth-context";
+import HomePage from "../pages/home-page";
+import ManagerCategoriesPage from "../pages/managercategories-page";
+import ManagerProductPage from "../pages/managerproduct-page";
+import OrderPage from "../pages/order-page";
+import ShoppingCartPage from "../pages/shoppingcart-page";
+import ManageClientPage from "../pages/manage-client";
+import RegisterPage from "../pages/register-page";
+import LgPage from "../pages/login-page";
+export default function RouterApp() {
+  return (
+    
+    <Router>
+<AuthProvider>
+        <Switch>
+        <Route exact path="/" component={HomePage}/>
+            <Route path="/register" component={RegisterPage}/>
+            <Route path="/admin" component={LgPage}/>
+            <Route path="/dash" component={DashboardPage}/>
+            <Route path="/categories" component={ManagerCategoriesPage} />
+            <Route path="/productes" component={ManagerProductPage} />
+            <Route path="/orders" component={OrderPage} />
+            <Route path="/shoppingcart" component={ShoppingCartPage} /> 
+            <Route path="/customers" component={ManageClientPage} />
+            <Route path="/manageClient" component={ManageClientPage} />
+            <Route path="*" component={Error404Page} />
+        </Switch>
+        </AuthProvider>
+    </Router>
+    
+  );
+}
